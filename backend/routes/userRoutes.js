c36af081e4abb69c39d6691aaf765a995b0f5f9b
Krishal-D <22151706@student.westernsydney.edu.au>
2025-09-28 11:57:31 +1000

    Update userRoutes: add login endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,17 +2,19 @@ import express from 'express'
 import {
     getUsers,
     getUserById,
-    createUsers,
     updateUsers,
     deleteUsers
 } from '../controllers/userController.js'
+
+
+
 
 const router = express.Router()
 
 router.get("/", getUsers)
 router.get("/:id",getUserById)
-router.post("/",createUsers)
 router.put("/:id",updateUsers)
 router.delete("/:id",deleteUsers)
+
 
 export default router
