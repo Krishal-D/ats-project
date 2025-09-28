f6308ef37afbbf206e5047e891b45823d1f00db7
Krishal-D <22151706@student.westernsydney.edu.au>
2025-09-28 11:57:31 +1000

    Update server.js: add auth routes and error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
-import { errorHandler } from './middleware/errorHandling.js'
+import  {errorHandler}  from './middleware/errorHandling.js'
+import authRoutes from './routes/authRoutes.js'
 
 const PORT = process.env.PORT || 5000
 
@@ -12,6 +13,8 @@ const app = express()
 app.use(express.json())
 
 app.use("/api/users", userRoutes)
+app.use("/api/auth",authRoutes)
+
 
 app.use(errorHandler)
 
