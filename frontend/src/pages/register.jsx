8ce2c83537a3c3f58d3a07648afbea3afcfc4d6c
Krishal-D <22151706@student.westernsydney.edu.au>
2025-09-30 23:58:21 +1000

    Implement handleNavigate, handleChange, and handleSubmit in Register page

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,13 +1,57 @@
 import { useNavigate } from "react-router-dom"
+import React from "react"
 
 export function Register() {
 
     const navigate = useNavigate()
 
-    function handleNavigate(){
+    function handleNavigate() {
         navigate("/login")
     }
 
+
+    const [form, setForm] = React.useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+        firstName: "",
+        lastName: ""
+    })
+
+    const handleChange = (e) => {
+        setForm(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault()
+
+        const values = {
+            ...form,
+            name: form.firstName + " " + form.lastName
+        }
+  
+
+        try {
+            const res = await fetch("http://localhost:5000/api/auth/register", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+                body: JSON.stringify(values)
+            })
+            const details = await res.json()
+            console.log(details)
+
+        } catch (err) {
+            console.error(err)
+        }
+
+
+
+    }
 
 
     return (
@@ -19,31 +63,31 @@ export function Register() {
                 <p className="formHeading">Register</p>
                 <p className="formMessage">Enter your credentials to create your account</p>
 
-                <form method="POST" className="form">
+                <form method="POST" onSubmit={handleSubmit} className="form">
 
                     <section className="name">
                         <div className="firstName">
                             <label htmlFor="firstName">First name</label>
-                            <input type="text" id="firstName" name="firstName" placeholder="John" required />
+                            <input type="text" id="firstName" name="firstName" value={form.firstName} onChange={handleChange} placeholder="John" required />
                         </div>
 
 
                         <div className="lastName">
                             <label htmlFor="lastName">Last name</label>
-                            <input type="text" id="lastName" name="lastName" placeholder="Cena" required />
+                            <input type="text" id="lastName" name="lastName" value={form.lastName} onChange={handleChange} placeholder="Cena" required />
                         </div>
 
                     </section>
 
 
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Enter your email" />
+                    <input type="email" name="email" id="email" value={form.email} onChange={handleChange} placeholder="Enter your email" />
 
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Enter your password" />
+                    <input type="password" name="password" id="password" value={form.password} onChange={handleChange} placeholder="Enter your password" />
 
                     <label htmlFor="confirmPassword">Confirm password</label>
-                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Comfirm your password" />
+                    <input type="password" name="confirmPassword" id="confirmPassword" value={form.confirmPassword} onChange={handleChange} placeholder="Comfirm your password" />
 
                     <label htmlFor="TC" >
                         <input type="checkbox" name="TC" id="TC" />
