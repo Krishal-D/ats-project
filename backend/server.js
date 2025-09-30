5b41725171af1fb32f00151b130a3b55bde06ed9
Krishal-D <22151706@student.westernsydney.edu.au>
2025-09-30 23:58:21 +1000

    Add CORS configuration to backend server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
-import  {errorHandler}  from './middleware/errorHandling.js'
+import { errorHandler } from './middleware/errorHandling.js'
 import authRoutes from './routes/authRoutes.js'
+import cors from "cors"
 
 const PORT = process.env.PORT || 5000
 
@@ -10,14 +11,27 @@ dotenv.config()
 
 const app = express()
 
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true
+    }));
+
+
+
+
 app.use(express.json())
 
+
+
+
+
 app.use("/api/users", userRoutes)
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes)
 
 
 app.use(errorHandler)
 
-app.listen("5000", () => {
+app.listen(PORT, () => {
     console.log(`Running at ${PORT}`)
 })
