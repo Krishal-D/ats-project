3f0c9ddc1404fbb6e3c5905e533c0af39ac99987
Krishal-D <22151706@student.westernsydney.edu.au>
2025-11-15 23:08:03 +1100

    fix(user): call editUser in updateUsers to persist profile changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,7 @@ export const updateUsers = async (req, res, next) => {
     const hashedPassword = password
       ? await bcrypt.hash(password, SALT_ROUNDS)
       : oldUser.password
-    const users = await (name, email, hashedPassword, id)
+    const users = await editUser(name, email, hashedPassword, id)
     res.status(200).json({ id: users.id, name: users.name, email: users.email })
   } catch (err) {
     next(err)
