de674537ea9bcb307c6ceefc6734eab457b0079a
Krishal-D <22151706@student.westernsydney.edu.au>
2025-12-02 16:48:10 +1100

    fix(setup): add AuthProvider wrapper to main component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 import App from './App.jsx'
+import { AuthProvider } from './auth/authContext.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 )
